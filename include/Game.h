1290e060c3a37bc695d8ea8a643b5be43d824f87
Daniel Silva <mail@dannyps.net>
2019-03-23 12:08:29 +0000

    A try at cmake

---
+++ include/Game.h
@@ -0,0 +1,19 @@
+/*
+ * Game.h
+ *
+ *  Created on: Mar 23, 2019
+ */
+
+#include <iostream>
+#include <string>
+
+#ifndef GAME_H_
+#define GAME_H_
+
+class Game {
+public:
+	Game::Game(int x, int y, std::string board);
+	virtual ~Game();
+};
+
+#endif /* GAME_H_ */
---
+++ src/Game.cpp
@@ -0,0 +1,17 @@
+/*
+ * Game.cpp
+ *
+ *  Created on: Mar 23, 2019
+ */
+
+#include "Game.h"
+using namespace std;
+
+Game::Game(int x, int y, string board) {
+
+}
+
+Game::~Game() {
+	// TODO Auto-generated destructor stub
+}
+
