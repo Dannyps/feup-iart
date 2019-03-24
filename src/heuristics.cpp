68ed63e758bc0c573ee2ded7e7d25f1133e692b3
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2019-03-24 17:30:22 +0000

    Simplify math heuristic (doesn't have to be a meaningful value)

--- src/heuristics.cpp
+++ src/heuristics.cpp
@@ -1,5 +1,5 @@
 #include "heuristics.h"
 
 double heuristicDiagonalDistance(Location state, Location target) {
-    return sqrt(pow(state.x-target.x,2)+pow(state.y-target.y,2));
+    return pow(state.x-target.x,2)+pow(state.y-target.y,2);
 }
